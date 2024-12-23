99f583b76bb91d04184ba736ee3d6e9ba5fa298a
SANDEEP BADETI <sandeepbadeti7730@gmail.com>
2024-12-23 14:06:45 +0530

    updated node.json

--- client/src/declaration.d.ts
+++ client/src/declaration.d.ts
@@ -2,8 +2,8 @@ declare module '*.css' {
     const content: { [className: string]: string };
     export default content;
   }
-declare module 'react-dom/client';
+declare module "node";
 declare module 'vite';
 declare module 'vite/client';
 declare module '@vitejs/plugin-react';
-declare module "node";
+declare module 'react-dom/client';
