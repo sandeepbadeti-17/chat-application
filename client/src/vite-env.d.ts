5ce823136556bd69f49e6f5be061769d3f1a664d
SANDEEP BADETI <sandeepbadeti7730@gmail.com>
2024-12-28 18:04:14 +0530

    changes in tsconfig.app.json

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,12 +1,3 @@
 /// <reference types="vite/client" />
 
-declare module 'vite' {
-    import { UserConfig } from 'vite';
-    export default UserConfig;
-  }
-  
-  declare module '@vitejs/plugin-react' {
-    const plugin: any;
-    export default plugin;
-  }
   
