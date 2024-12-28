32c8f39097384bff980df354e739babb06c37085
SANDEEP BADETI <sandeepbadeti7730@gmail.com>
2024-12-28 19:18:40 +0530

    changes in tsconfig.app.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react({ include: /\.(js|jsx|ts|tsx)$/ })],
 })
-
-declare module 'vite';
-declare module '@vitejs/plugin-react';
